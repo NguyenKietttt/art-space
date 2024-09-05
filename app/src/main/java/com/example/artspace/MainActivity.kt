87a663b6bce1cdf1149c012ef3752d7cd83edb55
Kndt <nkiet84@gmail.com>
2024-09-05 07:31:21 +0700

    Update UI

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.artspace
 
+import android.graphics.BlurMaskFilter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -20,12 +21,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,12 +49,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpaceLayout(modifier: Modifier = Modifier) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Red)
-    ) {
-        MainImage(modifier = modifier.weight(0.7f))
+    Column(modifier = Modifier.fillMaxSize()) {
+        MainImage(
+            modifier = modifier
+                .weight(0.7f)
+                .shadow(Color.Gray, 15.dp, 70.dp, 20.dp)
+        )
         ImageDescription(modifier = modifier.weight(0.2f))
         ControllerButtons(modifier = modifier.weight(0.1f))
     }
@@ -58,7 +66,7 @@ fun MainImage(modifier: Modifier = Modifier) {
         contentAlignment = Alignment.Center,
         modifier = modifier
             .fillMaxSize()
-            .padding(top = 72.dp, bottom = 16.dp, start = 16.dp, end = 16.dp)
+            .padding(top = 72.dp, bottom = 16.dp, start = 24.dp, end = 24.dp)
             .background(Color.White)
     ) {
         Image(
@@ -76,12 +84,13 @@ fun MainImage(modifier: Modifier = Modifier) {
 fun ImageDescription(modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp)
-            .background(Color.Cyan)
+            .padding(32.dp)
+            .background(Color(0xFFECEBF3))
     ) {
-        Text(text = "Kndt")
+        Text(text = "Avatar", fontSize = 24.sp, fontWeight = FontWeight.Bold)
         Text(text = "2017")
     }
 }
@@ -92,23 +101,17 @@ fun ControllerButtons(modifier: Modifier = Modifier) {
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier.fillMaxSize()
     ) {
-        Button(
-            modifier = Modifier
-                .padding(16.dp)
-                .weight(1.0f),
-            onClick = { /*TODO*/ })
-        {
+        Button(modifier = Modifier
+            .padding(16.dp)
+            .weight(1.0f), onClick = { /*TODO*/ }) {
             Text(text = "Previous")
         }
 
         Spacer(modifier = Modifier.width(10.dp))
 
-        Button(
-            modifier = Modifier
-                .padding(16.dp)
-                .weight(1.0f),
-            onClick = { /*TODO*/ }
-        ) {
+        Button(modifier = Modifier
+            .padding(16.dp)
+            .weight(1.0f), onClick = { /*TODO*/ }) {
             Text(text = "Next")
         }
     }
@@ -125,3 +128,35 @@ fun ArSpaceApp() {
         }
     }
 }
+
+fun Modifier.shadow(
+    color: Color = Color.Black,
+    offsetX: Dp = 0.dp,
+    offsetY: Dp = 0.dp,
+    blurRadius: Dp = 0.dp,
+) = then(
+    drawBehind {
+        drawIntoCanvas { canvas ->
+            val paint = Paint()
+            val frameworkPaint = paint.asFrameworkPaint()
+            if (blurRadius != 0.dp) {
+                frameworkPaint.maskFilter =
+                    (BlurMaskFilter(blurRadius.toPx(), BlurMaskFilter.Blur.NORMAL))
+            }
+            frameworkPaint.color = color.toArgb()
+
+            val leftPixel = offsetX.toPx()
+            val topPixel = offsetY.toPx()
+            val rightPixel = size.width - leftPixel
+            val bottomPixel = size.height
+
+            canvas.drawRect(
+                left = leftPixel,
+                top = topPixel,
+                right = rightPixel,
+                bottom = bottomPixel,
+                paint = paint,
+            )
+        }
+    }
+)
